d0cee18f6f9028927578f30531926661753c679d
Ericlys Moreira <ericlysm@gmail.com>
2023-04-13 18:50:09 -0300

    chore: check-ins controllers

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -3,12 +3,12 @@ import { z } from 'zod'
 import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
 
 export async function search(request: FastifyRequest, reply: FastifyReply) {
-  const searchGymBodySchema = z.object({
+  const searchGymQuerySchema = z.object({
     q: z.string(),
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { q, page } = searchGymBodySchema.parse(request.params)
+  const { q, page } = searchGymQuerySchema.parse(request.query)
 
   const searchGymsUseCase = makeSearchGymsUseCase()
 
