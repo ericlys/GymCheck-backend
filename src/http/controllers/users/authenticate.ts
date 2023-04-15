3191e7756a2c70278dc819ac10eea90e18699c65
Ericlys Moreira <ericlysm@gmail.com>
2023-04-15 14:50:42 -0300

    chore: add RBCA

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -22,11 +22,25 @@ export async function authenticate(
       password,
     })
 
-    const token = await reply.jwtSign({}, { sub: user.id })
+    const token = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      {
+        sub: user.id,
+      },
+    )
 
     const refreshToken = await reply.jwtSign(
-      {},
-      { sub: user.id, expiresIn: '7d' },
+      {
+        role: user.role,
+      },
+      {
+        sign: {
+          sub: user.id,
+          expiresIn: '7d',
+        },
+      },
     )
 
     return reply
